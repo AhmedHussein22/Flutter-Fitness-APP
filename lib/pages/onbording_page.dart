37d550f01053304596d049852369aa68d49d4dc6
ahmed <ah1901@fayoum.edu.eg>
2020-07-13 01:10:22 +0200

    fitness app first commit

---
+++ lib/pages/onbording_page.dart
@@ -0,0 +1,106 @@
+import 'package:test_gradle/pages/homePage.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnBording extends StatefulWidget {
+  @override
+  _OnBordingState createState() => _OnBordingState();
+}
+
+class _OnBordingState extends State<OnBording> {
+
+  var pages =[
+    PageViewModel(
+  title: "Habit",
+  body: "In order to succeed in fitness you must develop habits. Start one today....",
+ 
+  image: Center(child: Image.asset("assets/images/screen1.jpg", 
+  height: 500,
+  width: 700,
+  )
+  ),
+  decoration: const PageDecoration(
+    bodyTextStyle: TextStyle(fontSize: 15, color: Colors.white, ),
+    titleTextStyle:TextStyle(fontSize: 24, color: Color(0Xffff0e30),fontWeight: FontWeight.bold) ,
+
+    pageColor: Color(0Xff000725),
+  ),
+),
+
+
+PageViewModel(
+  title: "Discipline",
+  body: " Discipline comes from the willingness to learn, grow, and listen....",
+  image: Center(child: Image.asset("assets/images/screen2.png", height: 400,width: 500,)),
+  decoration: const PageDecoration(
+    bodyTextStyle: TextStyle(fontSize: 15, color: Colors.white, ),
+    titleTextStyle:TextStyle(fontSize: 24, color: Color(0Xffff0e30),fontWeight: FontWeight.bold) ,
+
+    pageColor:Color(0Xff000725),
+  ),
+),
+
+
+PageViewModel(
+  title: "Respect",
+  body: "it is just as courageous to stop at the right time as it is to fight through setbacks when the timing is right....",
+  image: Center(child: Image.asset("assets/images/screen3.png", height: 500,width: 500,)),
+   decoration: const PageDecoration(
+    bodyTextStyle: TextStyle(fontSize: 15, color: Colors.white, ),
+    titleTextStyle:TextStyle(fontSize: 24, color: Color(0Xffff0e30),fontWeight: FontWeight.bold) ,
+
+    pageColor:Color(0Xff000725),
+  ),
+),
+
+
+PageViewModel(
+  title: "Purpose",
+  body: "Purpose is the why behind your goal....",
+  image: Center(child: Image.asset("assets/images/screen4.png",height: 500,width: 500,)),
+  decoration: const PageDecoration(
+    bodyTextStyle: TextStyle(fontSize: 15, color: Colors.white, ),
+    titleTextStyle:TextStyle(fontSize: 24, color: Color(0Xffff0e30),fontWeight: FontWeight.bold) ,
+
+    pageColor: Color(0Xff000725),
+  ),
+),
+
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: IntroductionScreen(
+  pages: pages,
+  onDone: () {
+
+      Navigator.pushReplacement(context,
+       MaterialPageRoute(builder: (context)=> HomePage()),
+       );
+  },
+  onSkip: () {
+       Navigator.pushReplacement(context,
+       MaterialPageRoute(builder: (context)=> HomePage()),
+       );
+  },
+  showSkipButton: true,
+  skip: const Icon(Icons.cancel,color: Colors.white,size: 40,),
+  done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white)),
+  dotsDecorator: DotsDecorator(
+      size: const Size.square(10.0),
+      activeSize: const Size(20.0, 10.0),
+      activeColor: Colors.blue.shade800,
+      color: Colors.white,
+      spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+      activeShape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25.0)
+      )
+  ),
+)
+       
+      ),
+    );
+  }
+}
---
+++ lib/shared/error_image.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:test_gradle/pages/homePage.dart';
+
+class Error extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Color(0XFFff2fc3),
+      title: Text(
+        "OOPS ",
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 21,
+        ),
+      ),
+      content: Text(
+        "An error occurred during download...",
+        style: TextStyle(
+          color: Color(0Xff000725),
+          fontSize: 16,
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => HomePage()));
+            },
+            child: Text(
+              "OK",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 21,
+              ),
+            ))
+      ],
+    );
+  }
+}
+  
